0e147936528894c6d59604526e738562e1a9d8f2
Hoai2801 <dreamhoaihack@gmail.com>
2023-12-19 16:48:58 +0700

    countdown issue

--- src/pages/AutionDetail.js
+++ src/pages/AutionDetail.js
@@ -20,7 +20,7 @@ import { FaRegEye } from "react-icons/fa";
 import { FaRegHeart } from "react-icons/fa";
 import { TbTriangleSquareCircle } from "react-icons/tb";
 import axios from "axios";
-import {  Bars } from "react-loading-icons";
+import { Bars } from "react-loading-icons";
 const contractAddress = "0x5237bcc6f1848CDdF2785a12e1114Cd639895e36";
 
 const AutionDetail = () => {
@@ -30,15 +30,25 @@ const AutionDetail = () => {
   const address = useAddress();
   const [priceMATIC, setPrice] = useState();
 
+  // time countdown
   const [hoursRemaining, setHours] = useState();
   const [minutesRemaining, setMinutes] = useState();
   const [secondsRemaining, setSeconds] = useState();
+
+  // date end
+  const [days, setDays] = useState();
+  const [months, setMonths] = useState();
+  const [years, setYears] = useState();
+
+  const [winningBid, setWinningBid] = useState();
 
   // listing
   const [listing, setListing] = useState();
 
   // conver wei to ether
-  const price = listing ? ethers.utils.formatEther(listing.buyoutBidAmount) : "";
+  const price = listing
+    ? ethers.utils.formatEther(listing.buyoutBidAmount)
+    : "";
   const {
     mutateAsync: buyDirectListing,
     isLoading,
@@ -58,7 +68,6 @@ const AutionDetail = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        countdown(1702975992);
         const sdk = new ThirdwebSDK("mumbai", {
           clientId: "598b4f1195f15842446b09538ba00622",
         });
@@ -70,9 +79,19 @@ const AutionDetail = () => {
 
         // get listing detail
         const auction = await contractMarket.englishAuctions.getAuction(id);
-        console.log(auction)
+        countdown(auction.endTimeInSeconds);
+        // console.log(auction)
+
+        const winningBid = await contractMarket.englishAuctions.getWinningBid(
+          id
+        );
+        console.log(winningBid)
+        setWinningBid(winningBid);
 
         setListing(auction);
+
+        // change endTimeInSecond to date
+        convertSecondsToDate(auction.endTimeInSeconds);
 
         // api get MATIC price
         const price = await axios.get(
@@ -85,11 +104,22 @@ const AutionDetail = () => {
     };
 
     fetchData();
-  }, [id]);
+  }, [id, secondsRemaining]);
 
-  console.log(secondsRemaining)
-  console.log(minutesRemaining)
-  console.log(hoursRemaining)
+  // convert second to date
+  function convertSecondsToDate(seconds) {
+    // Tạo đối tượng Date từ dấu thời gian theo giây
+    const date = new Date(seconds * 1000);
+
+    // Lấy năm, tháng và ngày
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1; // Tháng được đánh chỉ mục từ 0, vì vậy hãy thêm 1
+    const day = date.getDate();
+
+    setDays(day);
+    setMonths(month);
+    setYears(year);
+  }
 
   // buy the nft
   const BuyNFT = async () => {
@@ -118,7 +148,8 @@ const AutionDetail = () => {
       const metadata = {
         name: "name",
         description: "description",
-        image: "https://images.unsplash.com/photo-1702611120121-e03dafc14150?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8", // This can be an image url or file
+        image:
+          "https://images.unsplash.com/photo-1702611120121-e03dafc14150?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8", // This can be an image url or file
       };
       const toAddress = "0xBDBA9d8889C6acFC3cEE850DC6DE393B01989D07";
 
@@ -139,22 +170,22 @@ const AutionDetail = () => {
   function countdown(endTimeInSeconds) {
     // Tạo đối tượng Date từ dấu thời gian theo giây
     const endTime = new Date(endTimeInSeconds * 1000);
-  
+
     // Lấy thời gian hiện tại
     const now = new Date();
-  
+
     // Tính toán thời gian còn lại
     const timeRemaining = endTime.getTime() - now.getTime();
-  
+
     // Chuyển đổi thời gian còn lại thành giờ, phút và giây
     const hoursRemaining = Math.floor(timeRemaining / (60 * 60));
     const minutesRemaining = Math.floor((timeRemaining % (60 * 60)) / 60);
     const secondsRemaining = timeRemaining % 60;
-  
+
     // Trả về các giá trị thời gian còn lại
-    setHours(hoursRemaining)
-    setMinutes(minutesRemaining)
-    setSeconds(secondsRemaining)
+    setHours(hoursRemaining);
+    setMinutes(minutesRemaining);
+    setSeconds(secondsRemaining);
   }
 
   const connectWallet = async () => {
@@ -163,8 +194,23 @@ const AutionDetail = () => {
 
   return (
     <div className="mt-5">
-      <div className={`${listing ? "hidden" : "d-flex justify-content-center align-items-center"}`} style={{ position: "fixed", top: 0, left: 0, right: 0, bottom: 0, backgroundColor: "rgba(1, 1, 1, 0.6)", zIndex: 9999 }}>
-        <Bars/>
+      <div
+        className={`${
+          listing
+            ? "hidden"
+            : "d-flex justify-content-center align-items-center"
+        }`}
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundColor: "rgba(1, 1, 1, 0.6)",
+          zIndex: 9999,
+        }}
+      >
+        <Bars />
       </div>
       <div className={`container-master-img-infor ${listing ? "" : "hidden"}`}>
         <div className="flex w-full px-20 gap-5 justify-between">
@@ -219,29 +265,38 @@ const AutionDetail = () => {
               <div className="">
                 <div className="card">
                   <div className="card-body">
-                    Aution ends 24 - 12 - 2023 at 7:05 CH
+                    Aution ends {days} - {months} - {years}
                     <div className="d-flex">
                       <div className="m-2">
-                        <div className="text-danger fs-4">13</div>
+                        <div className="text-danger fs-4">{hoursRemaining}</div>
                         <span>Hours</span>
                       </div>
                       <div className="m-2">
-                        <div className="text-danger fs-4">10</div>
+                        <div className="text-danger fs-4">
+                          {minutesRemaining}
+                        </div>
                         <span>Minutes</span>
                       </div>
                       <div className="m-2">
-                        <div className="text-danger fs-4">32</div>
+                        <div className="text-danger fs-4">
+                          {secondsRemaining}
+                        </div>
                         <span>Second</span>
                       </div>
                     </div>
                   </div>
                   <div className="card-footer">
                     <div>
-                      <span className="text-secondary d-block mb-4 " style={{fontSize:'18px'}}>Hightest price</span>
+                      <span
+                        className="text-secondary d-block mb-4 font-bold"
+                        style={{ fontSize: "18px" }}
+                      >
+                        Hightest bid belong to <span className="text-orange-400">{winningBid ? winningBid.bidderAddress : "nobody"} </span>
+                      </span>
                       <div className="d-flex align-items-center gap-4">
-                        <h1 className="fs-3">{price ? price : ""} MATIC</h1>
+                        <h1 className="fs-3">{winningBid ? winningBid.bidAmount / 1e18 : "0"} MATIC</h1>
                         <span className="text-secondary">
-                          ${price && priceMATIC ? price * priceMATIC : ""}
+                          ${winningBid && priceMATIC ? winningBid.bidAmount / 1e18 * priceMATIC : ""}
                         </span>
                       </div>
 
@@ -251,32 +306,22 @@ const AutionDetail = () => {
                             {listing && listing.creatorAddress === address ? (
                               <button
                                 onClick={() => cancelDirectListing(listing.id)}
-                                className="w-[80%] bg-[#0D6EFD] text-white rounded-l-lg"
+                                className="w-[90%] bg-[#0D6EFD] text-white rounded-lg h-[45px] font-bold"
                               >
-                                Cancel Listing
+                                Cancel Auction
                               </button>
                             ) : (
                               <button
                                 onClick={BuyNFT}
-                                className={`w-[90%] bg-[#0D6EFD] text-white rounded-lg h-[45px] font-bold cursor-wait  ${listing != null && listing.quantity == 0
-                                  ? " cursor-move"
-                                  : ""
-                                  }`}
+                                className={`w-[90%] bg-[#0D6EFD] text-white rounded-lg h-[45px] font-bold cursor-wait  ${
+                                  listing != null && listing.quantity == 0
+                                    ? "cursor-move"
+                                    : ""
+                                }`}
                               >
-                                {listing != null && listing.quantity == 0
-                                  ? "Sout out"
-                                  : "Make a bid"}
+                                Make a bid
                               </button>
                             )}
-
-                            {/* <span
-                              className="input-group-text btn btn-primary  p-2 fs-3"
-                              style={{ marginLeft: "1px" }}
-                              id="basic-addon1"
-                            >
-                              <IoMdCart />
-                            </span> */}
-                
                           </div>
                         </div>
                       </div>
