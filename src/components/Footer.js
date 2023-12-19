2b15f0537df43ce9aa8b787bb3779613228d5000
baconbao123 <anh450460@gmail.com>
2023-12-19 22:36:26 +0700

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,15 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Footer = () => {
   return(
 
-  <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
-      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+  <footer className="bg-white dark:bg-gray-900 m-4" >
+      <div className="w-full mx-auto p-4 md:py-8">
           <div className="sm:flex sm:items-center sm:justify-between">
-              <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+              <Link to="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                   {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" /> */}
                   <span className="self-center text-2xl font-semibold whitespace-nowrap">Simple W3</span>
-              </a>
+              </Link>
               <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                   <li>
                       <a href="#" className="hover:underline me-4 md:me-6">About</a>
